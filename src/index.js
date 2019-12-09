58adf70fd32f63a66430a40b6c33e979cf6cf0c3
LESANF <54767632+LESANF@users.noreply.github.com>
2019-12-09 16:45:45 +0900

    ING

--- src/index.js
+++ src/index.js
@@ -8,11 +8,59 @@ const form = document.querySelector(".js-form"),
   finishedList = document.querySelector(".js-finishedList");
 
 const TODOS_LS = "toDos";
+const FINISHED_LS = "FINISHED";
 let toDos = [];
+let finish = [];
 
-function transTodo() {}
+function transToDo(e) {
+  const trBtn = e.target;
+  const trBtnLi = e.target.parentNode;
+  pendingList.removeChild(trBtnLi);
 
-function deleTodo() {}
+  const trLi = document.createElement("li");
+  const trSpan = document.createElement("span");
+  const trDelBtn = document.createElement("button");
+  const RollbackBtn = document.createElement("button");
+  const trNewId = trBtnLi.id;
+  const trText = trBtnLi.firstChild.innerText;
+
+  trSpan.innerText = trText;
+  trDelBtn.innerText = "❌";
+  trDelBtn.addEventListener("click", deleToDo);
+  RollbackBtn.innerText = "⏪";
+  RollbackBtn.addEventListener("click", transToDo);
+
+  trLi.appendChild(trSpan);
+  trLi.appendChild(trDelBtn);
+  trLi.appendChild(RollbackBtn);
+  trLi.id = trNewId;
+  finishedList.appendChild(trLi);
+
+  const finObj = {
+    text: trText,
+    id: trNewId
+  };
+
+  finish.push(finObj);
+  saveToDos();
+}
+
+function deleToDo(e) {
+  const btn = e.target;
+  const btnLi = btn.parentNode;
+  pendingList.removeChild(btnLi);
+
+  const filterAry = toDos.filter(function(el) {
+    return el.id !== parseInt(btnLi.id);
+  });
+  toDos = filterAry;
+  saveToDos();
+}
+
+function saveToDos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+  localStorage.setItem(FINISHED_LS, JSON.stringify(finish));
+}
 
 function paintTodo(text) {
   const li = document.createElement("li");
@@ -20,15 +68,16 @@ function paintTodo(text) {
   const delBtn = document.createElement("button");
   const transBtn = document.createElement("button");
   const newId = toDos.length + 1;
-  span.innerText = "text ";
+  span.innerText = text;
   delBtn.innerText = "❌";
-  delBtn.addEventListener("click", deleTodo);
+  delBtn.addEventListener("click", deleToDo);
   transBtn.innerText = "✅";
-  delBtn.addEventListener("click", transTodo);
+  transBtn.addEventListener("click", transToDo);
 
   li.appendChild(span);
   li.appendChild(delBtn);
   li.appendChild(transBtn);
+  li.id = newId;
   pendingList.appendChild(li);
 
   const toDoObj = {
@@ -37,6 +86,7 @@ function paintTodo(text) {
   };
 
   toDos.push(toDoObj);
+  saveToDos();
 }
 
 function handeleSubmit(e) {
@@ -46,7 +96,18 @@ function handeleSubmit(e) {
   input.value = "";
 }
 
+function loadTodo() {
+  const current = localStorage.getItem(TODOS_LS);
+  if (current !== null) {
+    const parseCurrent = JSON.parse(current);
+    parseCurrent.forEach(function(el) {
+      paintTodo(el.text);
+    });
+  }
+}
+
 function init() {
+  loadTodo();
   form.addEventListener("submit", handeleSubmit);
 }
 
